bdba476d4ce3916d590ac2b16bf38bae3dc44d70
Sirajul Islam <ayasabegum134@gmail.com>
2024-04-06 13:47:17 +0600

    I create verify token for user authorization

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        'http://localhost:5174'
         // 'https://libary-manage.web.app',
         // 'https://libary-manage.firebaseapp.com'
     ],
